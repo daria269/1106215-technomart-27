dd585efc4f9d3466feeb6519481ffba9361335cb
daria <dasha_kravchenko_00@mail.ru>
2020-03-16 23:28:54 +0300

    поправила слайдер+мелкие исправления

---
+++ js/script-catalog.js
@@ -0,0 +1,113 @@
+"use strict";
+
+
+// $(function() {
+//   $("#slider-range").slider({
+//     range: true,
+//     min: 0,
+//     max: 35000,
+//     values: [0, 30000],
+//     slide: function(event, ui) {
+//       $("#amount1").val(ui.values[0]);
+//       $("#amount2").val(ui.values[1]);
+//     }
+//   });
+//   $("#amount1").val($("#slider-range").slider("values", 0));
+//   $("#amount2").val($("#slider-range").slider("values", 1));
+// });
+
+
+let modalAdded = document.querySelector('.modal-cart');
+let closeButton = document.querySelector('.close-modal-cart');
+let buyButton = document.querySelectorAll('.buy');
+let continueShopping = document.querySelector('.continueShopping');
+let cartNumber = document.querySelector('.cart-number');
+let counter = 0;
+
+
+
+[...buyButton].forEach(buyButton =>
+  buyButton.addEventListener('click', function() {
+    modalAdded.style.display = 'block';
+    if(cartNumber !== null){
+    counter++;
+    cartNumber.textContent = counter;
+  };
+  }));
+
+
+closeButton.onclick = function () {
+  modalAdded.style.display = 'none';
+}
+
+if (continueShopping !== null) {
+continueShopping.onclick = function () {
+  modalAdded.style.display = 'none';
+};
+}
+
+let sortPrice = document.querySelector('.sort-price');
+let sortType = document.querySelector('.sort-type');
+let sortFunctional = document.querySelector('.sort-functional');
+
+if (sortPrice !==null){
+sortPrice.onclick = function () {
+  sortPrice.classList.add('sorting-active');
+  sortType.classList.remove('sorting-active');
+  sortFunctional.classList.remove('sorting-active');
+};
+}
+if (sortType !== null) {
+sortType.onclick = function () {
+  sortType.classList.add('sorting-active');
+  sortPrice.classList.remove('sorting-active');
+  sortFunctional.classList.remove('sorting-active');
+};
+}
+
+if (sortFunctional !==null) {
+sortFunctional.onclick = function () {
+  sortFunctional.classList.add('sorting-active');
+  sortPrice.classList.remove('sorting-active');
+  sortType.classList.remove('sorting-active');
+};
+}
+
+let descending = document.querySelector('.descending');
+let ascending = document.querySelector('.ascending');
+
+if (descending !== null) {
+descending.onclick = function () {
+  descending.classList.add('sorting-active');
+  ascending.classList.remove('sorting-active');
+};
+}
+if (ascending !==null) {
+ascending.onclick = function () {
+  descending.classList.remove('sorting-active');
+  ascending.classList.add('sorting-active');
+};
+}
+
+
+let page1 = document.querySelector('.page1');
+let page2 = document.querySelector('.page2');
+let page3 = document.querySelector('.page3');
+
+page1.onclick = function () {
+  page1.classList.add('active-page');
+  page2.classList.remove('active-page');
+  page3.classList.remove('active-page');
+};
+
+page2.onclick = function () {
+  page1.classList.remove('active-page');
+  page2.classList.add('active-page');
+  page3.classList.remove('active-page');
+};
+
+page3.onclick = function () {
+  page1.classList.remove('active-page');
+  page2.classList.remove('active-page');
+  page3.classList.add('active-page');
+};
--- js/script-index.js
+++ js/script-index.js
@@ -1,33 +1,30 @@
 "use strict";
 
 
-
-
-
 let mapMini = document.querySelector('.map-mini');
 let mapOpen = document.querySelector('.map-open');
 
 if (mapOpen !== null) {
-mapOpen.onclick = function() {
-  mapMini.style.overflow = 'visible';
-};
+  mapOpen.onclick = function() {
+    mapMini.style.overflow = 'visible';
+  };
 };
 
 let closeMap = document.querySelector('.close-map');
 
 if (closeMap !== null) {
-closeMap.onclick = function() {
-  mapMini.style.overflow = 'hidden';
-};
+  closeMap.onclick = function() {
+    mapMini.style.overflow = 'hidden';
+  };
 };
 
 let modal = document.querySelector('.modal-write-us');
 let writeBtn = document.querySelector('.long-button');
 
 if (writeBtn !== null) {
-writeBtn.onclick = function() {
-  modal.style.display = 'block';
-};
+  writeBtn.onclick = function() {
+    modal.style.display = 'block';
+  };
 };
 
 let closeModal = document.querySelector('.close-button');
@@ -51,55 +48,28 @@ let indicator1 = document.querySelector('.indicator1');
 let indicator2 = document.querySelector('.indicator2');
 
 
-let left = 0;
-let frame = 620;
 
-if (leftButton !== null) {
 leftButton.onclick = moveLeft;
-};
-
-if (rightButton !== null) {
 rightButton.onclick = moveRight;
-};
+indicator1.onclick = moveLeft;
+indicator2.onclick = moveRight;
+
 
 function moveRight() {
   let element = document.querySelector(".slider-field>div:first-child");
   sliderField.appendChild(element);
-  element.classList.add('for-right');
   indicator1.classList.toggle('indicator-active');
   indicator2.classList.toggle('indicator-active');
-
-
 }
 
 function moveLeft() {
   let element = document.querySelector(".slider-field>div:last-child");
   let firstElement = document.querySelector(".slider-field>div:first-child");
   sliderField.appendChild(firstElement, element);
-   firstElement,element.classList.add('for-left');
-   indicator2.classList.toggle('indicator-active');
-   indicator1.classList.toggle('indicator-active');
-
-
+  indicator2.classList.toggle('indicator-active');
+  indicator1.classList.toggle('indicator-active');
 }
 
-
-//
-// function moveLeft() {
-//   left = left - frame;
-//   if (left < -frame) {
-//     // left = 0;
-//   }
-//   sliderField.style.left = left + 'px';
-// }
-//
-// function moveRight() {
-//   left = left + frame;
-//   if (left > 0) {
-//     left = -frame;
-//   }
-//   sliderField.style.left = left + 'px';
-// }
 
 let deliveryBtn = document.getElementById('delivery-li');
 let guarantBtn = document.getElementById('guarantee-li');
@@ -112,13 +82,13 @@ let background = document.querySelector('.service-detail');
 
 
 if (deliveryBtn !== null) {
-deliveryBtn.onclick = showDelivery;
+  deliveryBtn.onclick = showDelivery;
 };
 if (guarantBtn !== null) {
-guarantBtn.onclick = showGuarant;
+  guarantBtn.onclick = showGuarant;
 };
 if (creditBtn !== null) {
-creditBtn.onclick = showCredit;
+  creditBtn.onclick = showCredit;
 };
 
 
@@ -171,99 +141,39 @@ function showCredit() {
 }
 
 
-$(function() {
-  $("#slider-range").slider({
-    range: true,
-    min: 0,
-    max: 35000,
-    values: [0, 30000],
-    slide: function(event, ui) {
-      $("#amount1").val(ui.values[0]);
-      $("#amount2").val(ui.values[1]);
-    }
-  });
-  $("#amount1").val($("#slider-range").slider("values", 0));
-  $("#amount2").val($("#slider-range").slider("values", 1));
-});
+// $(function() {
+//   $("#slider-range").slider({
+//     range: true,
+//     min: 0,
+//     max: 35000,
+//     values: [0, 30000],
+//     slide: function(event, ui) {
+//       $("#amount1").val(ui.values[0]);
+//       $("#amount2").val(ui.values[1]);
+//     }
+//   });
+//   $("#amount1").val($("#slider-range").slider("values", 0));
+//   $("#amount2").val($("#slider-range").slider("values", 1));
+// });
 
 
 let modalAdded = document.querySelector('.modal-cart');
-let closeButton = document.querySelector('.close-button');
+let closeButton = document.querySelector('.close-modal-cart');
 let buyButton = document.querySelectorAll('.buy');
 let continueShopping = document.querySelector('.continueShopping');
-let cartNumber = document.querySelector('.cart-number');
-let counter = 0;
-
-
 
 [...buyButton].forEach(buyButton =>
   buyButton.addEventListener('click', function() {
     modalAdded.style.display = 'block';
-    counter++;
-    cartNumber.textContent = counter;
   }));
 
-
-closeButton.onclick = function () {
+closeButton.onclick = function() {
   modalAdded.style.display = 'none';
+};
+
+
+if (continueShopping !== null) {
+  continueShopping.onclick = function() {
+    modalAdded.style.display = 'none';
+  };
 }
-
-continueShopping.onclick = function () {
-  modalAdded.style.display = 'none';
-}
-
-let sortPrice = document.querySelector('.sort-price');
-let sortType = document.querySelector('.sort-type');
-let sortFunctional = document.querySelector('.sort-functional');
-
-sortPrice.onclick = function () {
-  sortPrice.classList.add('sorting-active');
-  sortType.classList.remove('sorting-active');
-  sortFunctional.classList.remove('sorting-active');
-};
-sortType.onclick = function () {
-  sortType.classList.add('sorting-active');
-  sortPrice.classList.remove('sorting-active');
-  sortFunctional.classList.remove('sorting-active');
-};
-sortFunctional.onclick = function () {
-  sortFunctional.classList.add('sorting-active');
-  sortPrice.classList.remove('sorting-active');
-  sortType.classList.remove('sorting-active');
-};
-
-let descending = document.querySelector('.descending');
-let ascending = document.querySelector('.ascending');
-
-descending.onclick = function () {
-  descending.classList.add('sorting-active');
-  ascending.classList.remove('sorting-active');
-};
-
-ascending.onclick = function () {
-  descending.classList.remove('sorting-active');
-  ascending.classList.add('sorting-active');
-};
-
-
-let page1 = document.querySelector('.page1');
-let page2 = document.querySelector('.page2');
-let page3 = document.querySelector('.page3');
-
-page1.onclick = function () {
-  page1.classList.add('active-page');
-  page2.classList.remove('active-page');
-  page3.classList.remove('active-page');
-};
-
-page2.onclick = function () {
-  page1.classList.remove('active-page');
-  page2.classList.add('active-page');
-  page3.classList.remove('active-page');
-};
-
-page3.onclick = function () {
-  page1.classList.remove('active-page');
-  page2.classList.remove('active-page');
-  page3.classList.add('active-page');
-};
