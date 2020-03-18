ae8a134ddb8f156c2f8fe696b55eab9a20ae6a3a
daria <dasha_kravchenko_00@mail.ru>
2020-03-19 01:22:43 +0300

    конечный вариант

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -1,6 +1,68 @@
 "use strict";
 
 
+let thumbLeft = document.querySelector(".range-thumb-left");
+let thumbRight = document.querySelector(".range-thumb-right");
+let sliderRange = document.querySelector("#slider-range");
+
+thumbLeft.onmousedown = function(event) {
+  event.preventDefault();
+  let shiftX = event.clientX - thumbLeft.getBoundingClientRect().left;
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  function onMouseMove(event) {
+    let newLeft = event.clientX - shiftX - sliderRange.getBoundingClientRect().left;
+    //оставить бегунок в  границах.
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    let rightEdge = document.offsetWidth - thumbLeft.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+    thumbLeft.style.left = newLeft + "px";
+  }
+
+  function onMouseUp() {
+    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("mousemove", onMouseMove);
+  }
+};
+
+thumbRight.onmousedown = function(event) {
+  event.preventDefault();
+  let shiftX = event.clientX - thumbRight.getBoundingClientRect().left;
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  function onMouseMove(event) {
+    let newLeft = event.clientX - shiftX - sliderRange.getBoundingClientRect().left;
+    //оставить бегунок в  границах.
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    let rightEdge = document.offsetWidth - thumbRight.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+    thumbRight.style.left = newLeft + "px";
+  }
+
+  function onMouseUp() {
+    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("mousemove", onMouseMove);
+  }
+
+};
+
+thumbLeft.ondragstart = function() {
+  return false;
+};
+thumbRight.ondragstart = function() {
+  return false;
+};
+
 // $(function() {
 //   $("#slider-range").slider({
 //     range: true,
@@ -17,97 +79,97 @@
 // });
 
 
-let modalAdded = document.querySelector('.modal-cart');
-let closeButton = document.querySelector('.close-modal-cart');
-let buyButton = document.querySelectorAll('.buy');
-let continueShopping = document.querySelector('.continueShopping');
-let cartNumber = document.querySelector('.cart-number');
+let modalAdded = document.querySelector(".modal-cart");
+let closeButton = document.querySelector(".close-modal-cart");
+let buyButton = document.querySelectorAll(".buy");
+let continueShopping = document.querySelector(".continueShopping");
+let cartNumber = document.querySelector(".cart-number");
 let counter = 0;
 
 
 
 [...buyButton].forEach(buyButton =>
-  buyButton.addEventListener('click', function() {
-    modalAdded.style.display = 'block';
-    if(cartNumber !== null){
-    counter++;
-    cartNumber.textContent = counter;
-  };
+  buyButton.addEventListener("click", function() {
+    modalAdded.style.display = "block";
+    if (cartNumber !== null) {
+      counter++;
+      cartNumber.textContent = counter;
+    };
   }));
 
 
-closeButton.onclick = function () {
-  modalAdded.style.display = 'none';
+closeButton.onclick = function() {
+  modalAdded.style.display = "none";
 }
 
 if (continueShopping !== null) {
-continueShopping.onclick = function () {
-  modalAdded.style.display = 'none';
-};
+  continueShopping.onclick = function() {
+    modalAdded.style.display = "none";
+  };
 }
 
-let sortPrice = document.querySelector('.sort-price');
-let sortType = document.querySelector('.sort-type');
-let sortFunctional = document.querySelector('.sort-functional');
+let sortPrice = document.querySelector(".sort-price");
+let sortType = document.querySelector(".sort-type");
+let sortFunctional = document.querySelector(".sort-functional");
 
-if (sortPrice !==null){
-sortPrice.onclick = function () {
-  sortPrice.classList.add('sorting-active');
-  sortType.classList.remove('sorting-active');
-  sortFunctional.classList.remove('sorting-active');
-};
+if (sortPrice !== null) {
+  sortPrice.onclick = function() {
+    sortPrice.classList.add("sorting-active");
+    sortType.classList.remove("sorting-active");
+    sortFunctional.classList.remove("sorting-active");
+  };
 }
 if (sortType !== null) {
-sortType.onclick = function () {
-  sortType.classList.add('sorting-active');
-  sortPrice.classList.remove('sorting-active');
-  sortFunctional.classList.remove('sorting-active');
-};
+  sortType.onclick = function() {
+    sortType.classList.add("sorting-active");
+    sortPrice.classList.remove("sorting-active");
+    sortFunctional.classList.remove("sorting-active");
+  };
 }
 
-if (sortFunctional !==null) {
-sortFunctional.onclick = function () {
-  sortFunctional.classList.add('sorting-active');
-  sortPrice.classList.remove('sorting-active');
-  sortType.classList.remove('sorting-active');
-};
+if (sortFunctional !== null) {
+  sortFunctional.onclick = function() {
+    sortFunctional.classList.add("sorting-active");
+    sortPrice.classList.remove("sorting-active");
+    sortType.classList.remove("sorting-active");
+  };
 }
 
-let descending = document.querySelector('.descending');
-let ascending = document.querySelector('.ascending');
+let descending = document.querySelector(".descending");
+let ascending = document.querySelector(".ascending");
 
 if (descending !== null) {
-descending.onclick = function () {
-  descending.classList.add('sorting-active');
-  ascending.classList.remove('sorting-active');
-};
+  descending.onclick = function() {
+    descending.classList.add("sorting-active");
+    ascending.classList.remove("sorting-active");
+  };
 }
-if (ascending !==null) {
-ascending.onclick = function () {
-  descending.classList.remove('sorting-active');
-  ascending.classList.add('sorting-active');
-};
+if (ascending !== null) {
+  ascending.onclick = function() {
+    descending.classList.remove("sorting-active");
+    ascending.classList.add("sorting-active");
+  };
 }
 
 
-let page1 = document.querySelector('.page1');
-let page2 = document.querySelector('.page2');
-let page3 = document.querySelector('.page3');
+let page1 = document.querySelector(".page1");
+let page2 = document.querySelector(".page2");
+let page3 = document.querySelector(".page3");
 
-page1.onclick = function () {
-  page1.classList.add('active-page');
-  page2.classList.remove('active-page');
-  page3.classList.remove('active-page');
+page1.onclick = function() {
+  page1.classList.add("active-page");
+  page2.classList.remove("active-page");
+  page3.classList.remove("active-page");
 };
 
-page2.onclick = function () {
-  page1.classList.remove('active-page');
-  page2.classList.add('active-page');
-  page3.classList.remove('active-page');
+page2.onclick = function() {
+  page1.classList.remove("active-page");
+  page2.classList.add("active-page");
+  page3.classList.remove("active-page");
 };
 
-page3.onclick = function () {
-  page1.classList.remove('active-page');
-  page2.classList.remove('active-page');
-  page3.classList.add('active-page');
+page3.onclick = function() {
+  page1.classList.remove("active-page");
+  page2.classList.remove("active-page");
+  page3.classList.add("active-page");
 };
--- js/script-index.js
+++ js/script-index.js
@@ -1,51 +1,51 @@
 "use strict";
 
 
-let mapMini = document.querySelector('.map-mini');
-let mapOpen = document.querySelector('.map-open');
+let mapMini = document.querySelector(".map-mini");
+let mapOpen = document.querySelector(".map-open");
 
 if (mapOpen !== null) {
   mapOpen.onclick = function() {
-    mapMini.style.overflow = 'visible';
+    mapMini.style.overflow = "visible";
   };
 };
 
-let closeMap = document.querySelector('.close-map');
+let closeMap = document.querySelector(".close-map");
 
 if (closeMap !== null) {
   closeMap.onclick = function() {
-    mapMini.style.overflow = 'hidden';
+    mapMini.style.overflow = "hidden";
   };
 };
 
-let modal = document.querySelector('.modal-write-us');
-let writeBtn = document.querySelector('.long-button');
+let modal = document.querySelector(".modal-write-us");
+let writeBtn = document.querySelector(".long-button");
 
 if (writeBtn !== null) {
   writeBtn.onclick = function() {
-    modal.style.display = 'block';
+    modal.style.display = "block";
   };
 };
 
-let closeModal = document.querySelector('.close-button');
+let closeModal = document.querySelector(".close-button");
 closeModal.onclick = function() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 };
 
 /* window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display ='none';
+    modal.style.display ="none";
   };
 }
 не работает */
 
-let sliderField = document.querySelector('.slider-field');
-let leftButton = document.querySelector('.slide-left');
-let rightButton = document.querySelector('.slide-right');
-let slide1 = document.querySelector('.slide1');
-let slide2 = document.querySelector('.slide2');
-let indicator1 = document.querySelector('.indicator1');
-let indicator2 = document.querySelector('.indicator2');
+let sliderField = document.querySelector(".slider-field");
+let leftButton = document.querySelector(".slide-left");
+let rightButton = document.querySelector(".slide-right");
+let slide1 = document.querySelector(".slide1");
+let slide2 = document.querySelector(".slide2");
+let indicator1 = document.querySelector(".indicator1");
+let indicator2 = document.querySelector(".indicator2");
 
 
 
@@ -58,27 +58,27 @@ indicator2.onclick = moveRight;
 function moveRight() {
   let element = document.querySelector(".slider-field>div:first-child");
   sliderField.appendChild(element);
-  indicator1.classList.toggle('indicator-active');
-  indicator2.classList.toggle('indicator-active');
+  indicator1.classList.toggle("indicator-active");
+  indicator2.classList.toggle("indicator-active");
 }
 
 function moveLeft() {
   let element = document.querySelector(".slider-field>div:last-child");
   let firstElement = document.querySelector(".slider-field>div:first-child");
   sliderField.appendChild(firstElement, element);
-  indicator2.classList.toggle('indicator-active');
-  indicator1.classList.toggle('indicator-active');
+  indicator2.classList.toggle("indicator-active");
+  indicator1.classList.toggle("indicator-active");
 }
 
 
-let deliveryBtn = document.getElementById('delivery-li');
-let guarantBtn = document.getElementById('guarantee-li');
-let creditBtn = document.getElementById('credit-li');
-let guarantDiv = document.querySelector('.guarantee');
-let deliveryDiv = document.querySelector('.delivery');
-let creditDiv = document.querySelector('.credit');
+let deliveryBtn = document.getElementById("delivery-li");
+let guarantBtn = document.getElementById("guarantee-li");
+let creditBtn = document.getElementById("credit-li");
+let guarantDiv = document.querySelector(".guarantee");
+let deliveryDiv = document.querySelector(".delivery");
+let creditDiv = document.querySelector(".credit");
 
-let background = document.querySelector('.service-detail');
+let background = document.querySelector(".service-detail");
 
 
 if (deliveryBtn !== null) {
@@ -93,51 +93,51 @@ if (creditBtn !== null) {
 
 
 function showDelivery() {
-  guarantDiv.style.display = 'none';
-  creditDiv.style.display = 'none';
-  deliveryDiv.style.display = 'block';
-  background.classList.remove('guarantee-icon');
-  background.classList.remove('credit-icon');
-  background.classList.add('delivery-icon');
-  deliveryBtn.style.backgroundColor = '#ffffff';
-  deliveryBtn.style.color = '#32425c';
-  deliveryBtn.style.boxShadow = 'none';
-  deliveryBtn.style.border = '#ffffff';
-  guarantBtn.style = 'none';
-  creditBtn.style = 'none';
+  guarantDiv.style.display = "none";
+  creditDiv.style.display = "none";
+  deliveryDiv.style.display = "block";
+  background.classList.remove("guarantee-icon");
+  background.classList.remove("credit-icon");
+  background.classList.add("delivery-icon");
+  deliveryBtn.style.backgroundColor = "#ffffff";
+  deliveryBtn.style.color = "#32425c";
+  deliveryBtn.style.boxShadow = "none";
+  deliveryBtn.style.border = "#ffffff";
+  guarantBtn.style = "none";
+  creditBtn.style = "none";
 }
 
 function showGuarant() {
 
-  deliveryDiv.style.display = 'none';
-  creditDiv.style.display = 'none';
-  guarantDiv.style.display = 'block';
-  background.classList.remove('delivery-icon');
-  background.classList.remove('credit-icon');
-  background.classList.add('guarantee-icon');
-  guarantBtn.style.backgroundColor = '#ffffff';
-  guarantBtn.style.color = '#32425c';
-  guarantBtn.style.boxShadow = 'none';
-  guarantBtn.style.border = '#ffffff';
-  deliveryBtn.style = 'none';
-  creditBtn.style = 'none';
+  deliveryDiv.style.display = "none";
+  creditDiv.style.display = "none";
+  guarantDiv.style.display = "block";
+  background.classList.remove("delivery-icon");
+  background.classList.remove("credit-icon");
+  background.classList.add("guarantee-icon");
+  guarantBtn.style.backgroundColor = "#ffffff";
+  guarantBtn.style.color = "#32425c";
+  guarantBtn.style.boxShadow = "none";
+  guarantBtn.style.border = "#ffffff";
+  deliveryBtn.style = "none";
+  creditBtn.style = "none";
 
 }
 
 function showCredit() {
 
-  deliveryDiv.style.display = 'none';
-  guarantDiv.style.display = 'none';
-  creditDiv.style.display = 'block';
-  background.classList.remove('delivery-icon');
-  background.classList.add('credit-icon');
-  background.classList.remove('guarantee-icon');
-  creditBtn.style.backgroundColor = '#ffffff';
-  creditBtn.style.color = '#32425c';
-  creditBtn.style.boxShadow = 'none';
-  creditBtn.style.border = '#ffffff';
-  deliveryBtn.style = 'none';
-  guarantBtn.style = 'none';
+  deliveryDiv.style.display = "none";
+  guarantDiv.style.display = "none";
+  creditDiv.style.display = "block";
+  background.classList.remove("delivery-icon");
+  background.classList.add("credit-icon");
+  background.classList.remove("guarantee-icon");
+  creditBtn.style.backgroundColor = "#ffffff";
+  creditBtn.style.color = "#32425c";
+  creditBtn.style.boxShadow = "none";
+  creditBtn.style.border = "#ffffff";
+  deliveryBtn.style = "none";
+  guarantBtn.style = "none";
 }
 
 
@@ -157,23 +157,23 @@ function showCredit() {
 // });
 
 
-let modalAdded = document.querySelector('.modal-cart');
-let closeButton = document.querySelector('.close-modal-cart');
-let buyButton = document.querySelectorAll('.buy');
-let continueShopping = document.querySelector('.continueShopping');
+let modalAdded = document.querySelector(".modal-cart");
+let closeButton = document.querySelector(".close-modal-cart");
+let buyButton = document.querySelectorAll(".buy");
+let continueShopping = document.querySelector(".continueShopping");
 
 [...buyButton].forEach(buyButton =>
-  buyButton.addEventListener('click', function() {
-    modalAdded.style.display = 'block';
+  buyButton.addEventListener("click", function() {
+    modalAdded.style.display = "block";
   }));
 
 closeButton.onclick = function() {
-  modalAdded.style.display = 'none';
+  modalAdded.style.display = "none";
 };
 
 
 if (continueShopping !== null) {
   continueShopping.onclick = function() {
-    modalAdded.style.display = 'none';
+    modalAdded.style.display = "none";
   };
 }
